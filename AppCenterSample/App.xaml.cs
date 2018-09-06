07527a146f20c9af71ce2c303f90f40cf9fa4c7f
Jim Bennett <jim.bennett@microsoft.com>
2018-09-06 20:21:50 +0100

    UI tweaks

--- AppCenterSample/App.xaml.cs
+++ AppCenterSample/App.xaml.cs
@@ -15,12 +15,17 @@ namespace AppCenterSample
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            var mainPage = new MainPage();
+            var navPage = new NavigationPage(mainPage);
+            navPage.BarTextColor = Color.White;
+            MainPage = navPage;
+
+            NavigationPage.SetHasNavigationBar(mainPage, false);
         }
 
         protected override void OnStart()
         {
-            AppCenter.Start("ios=21211fcd-f4af-464c-abf3-6d1004392e3f;" +
+            AppCenter.Start("ios=acee40cf-58cd-421c-a4f5-dba0683a4df9;" +
                             "android=68132a34-9578-4a61-bf5c-fbd10ca6f9cc",
                             typeof(Analytics), typeof(Crashes), typeof(Distribute));
         }
